ebcfc008acd3268fee62ad1b9128afc3aa734473
Arindam01gi <arindammaiti2018@gmail.com>
2024-09-03 23:08:40 +0530

    update in projects

--- src/container/Project.jsx
+++ src/container/Project.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const Project = () => {
   return (
-    <div className='h-screen bg-blue-100'>Project</div>
+    <div className='pt-8 flex justify-center items-center bg-blue-100 py-8'>
+      <div className='w-5/6 '>
+
+        <h1 className='text-4xl font-extrabold text-body-blue tracking-wide' style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Projects</h1>
+
+        <div className='py-6 flex justify-center gap-6'>
+            <div className='py-2 px-5 shadow-lg rounded-lg bg-white cursor-pointer'>
+                  All
+            </div>
+            <div className='py-2 px-5 shadow-lg rounded-lg bg-white cursor-pointer'>
+                  Frontend 
+            </div>
+            <div className='py-2 px-5 shadow-lg rounded-lg bg-white cursor-pointer'>
+                  Backend
+            </div>
+            <div className='py-2 px-5 shadow-lg rounded-lg bg-white cursor-pointer'>
+                  FullStack
+            </div>
+        </div>
+
+        <div className='flex justify-evenly  flex-wrap'>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+        </div>
+
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/container/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectCard = () => {
+  return (
+    <div className='w-80 h-80 shadow-lg bg-white rounded-lg my-6'>
+       Card
+
+    </div>
+  )
+}
+
+export default ProjectCard
